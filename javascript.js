180cfc13604a04a930f9c7b706eccbd75239599d
Raj-keshav <itsrajkeshav@gmail.com>
2024-02-23 15:17:26 +0530

    fully functional

--- javascript.js
+++ javascript.js
@@ -12,3 +12,11 @@ function cl() {
 function del() {
   display.value = display.value.substring(0, display.value.length - 1);
 }
+function solve() {
+  // display.value += value;
+  // display.value = display.value + display.value;
+  let x = display.value;
+  let y = eval(x);
+  display.value = y;
+  return y;
+}
